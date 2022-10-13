8519d55264b82c58a4f68e7c8c3c1f261051af03
Alessandro <tryit0utspectre@gmail.com>
2022-10-13 23:15:36 +0200

    Submit exercise number #5

---
+++ mediaVoti/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace es5_mediaVoti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcola_Click(object sender, EventArgs e)
+        {
+            string nome;
+            float voto;
+
+            float sommavoti = 0;
+
+            float mediaStud;
+            float mediaClasse;
+
+            float sommaMedia = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                nome = Interaction.InputBox("Inserire nome per alunno numero " + i);
+                lstNomi.Items.Add(nome);
+                for (int j = 1; j <= 8; j++)
+                {
+                    voto = Convert.ToSingle(Interaction.InputBox("Inserire il voto della materia numero " + j + " per l'alunno numero " + i));
+                    sommavoti += voto;
+                }
+                mediaStud = sommavoti / 8;
+                lstMedie.Items.Add(mediaStud);
+                sommavoti = 0;
+                sommaMedia += mediaStud;
+            }
+            mediaClasse = sommaMedia / 20;
+            txtMediaClasse.Text = mediaClasse.ToString();
+        }
+    }
+}
